b05526f76f8949de8e88f3ecc9d1d770c09c7f8e
William Bert <william.bert@gmail.com>
2013-11-09 15:46:54 -0500

    Set environment.

--- app.js
+++ app.js
@@ -11,11 +11,21 @@ var path = require('path');
 
 var app = express();
 
+// Set environment.
+if(process.env.OPENSHIFT_NODEJS_IP) app.set('env', 'production');
+
 // all environments
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+// Middleware.
+app.use(function(req, res, next) {
+    res.locals.env = app.get('env');
+    next();
+});
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -23,13 +33,15 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// Development only.
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Routing.
 app.get('/', routes.index);
 
+// Server.
 console.log("binding to: " + app.get('ip') + ":" + app.get('port'));
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
